5b1615c725a208a93941e897bf5ffd5ba6709641
yjthay <thayyeejie@gmail.com>
2020-05-17 17:19:23 +0100

    Created the first version of golang api server with a fixed set of instruments to create at initiation.
    Will need to update vanillaOpt.go to create a more complex structure for the output of the API

---
+++ data/golang/api.go
@@ -0,0 +1,114 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"strconv"
+
+	"github.com/gorilla/mux"
+	"github.com/yjthay/ib_dashboard/data/pricer"
+)
+
+var portfolio []pricer.VanillaOption
+
+func main() {
+	router := mux.NewRouter()
+	portfolio = append(portfolio, pricer.VanillaOption{Strike: 280, Spot: 280, RiskFreeRate: 0.05,
+		Price: 39.847556, EvalDate: "20200517", ExpDate: "20210517", Type: "C"})
+	portfolio = append(portfolio, pricer.VanillaOption{Strike: 280, Spot: 280, RiskFreeRate: 0.05,
+		Price: 26.191795, EvalDate: "20200517", ExpDate: "20210517", Type: "P"})
+	router.HandleFunc("/api/opts", getAllOpts).Methods("GET")
+	router.HandleFunc("/api/opts/{id}", getOpt).Methods("GET")
+	router.HandleFunc("/api/opts", createOpt).Methods("POST")
+	router.HandleFunc("/api/opts/{id}", deleteOpt).Methods("DELETE")
+	router.HandleFunc("/api/opts/{id}", updateOpt).Methods("PUT")
+
+	fmt.Println("Starting up server...")
+	log.Fatal(http.ListenAndServe(":7777", router))
+}
+
+// Get all options
+func getAllOpts(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	for id, opt := range portfolio {
+		output := *pricer.Opt(opt.Type, opt.EvalDate, opt.ExpDate,
+			opt.Strike, opt.Spot, opt.RiskFreeRate, opt.Q, opt.Price, -1)
+		portfolio[id] = output
+	}
+	json.NewEncoder(w).Encode(portfolio)
+	return
+}
+
+// Get a single specific option
+func getOpt(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	id, err := strconv.Atoi(params["id"])
+	if err != nil {
+		w.WriteHeader(400)
+		w.Write([]byte("ID could not be converted to integer"))
+		return
+	}
+	if id >= len(portfolio) {
+		w.WriteHeader(404)
+		w.Write([]byte("No option found with specified ID"))
+		return
+	}
+	for id, opt := range portfolio {
+		output := *pricer.Opt(opt.Type, opt.EvalDate, opt.ExpDate,
+			opt.Strike, opt.Spot, opt.RiskFreeRate, opt.Q, opt.Price, -1)
+		portfolio[id] = output
+	}
+	opt := portfolio[id]
+	json.NewEncoder(w).Encode(opt)
+}
+
+// Create an option
+func createOpt(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	var opt pricer.VanillaOption
+	json.NewDecoder(r.Body).Decode(&opt)
+	portfolio = append(portfolio, opt)
+	json.NewEncoder(w).Encode(opt)
+}
+
+// Delete an option
+func deleteOpt(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	id, err := strconv.Atoi(params["id"])
+	if err != nil {
+		w.WriteHeader(400)
+		w.Write([]byte("ID could not be converted to integer"))
+		return
+	}
+	if id >= len(portfolio) {
+		w.WriteHeader(404)
+		w.Write([]byte("No option found with specified ID"))
+		return
+	}
+	portfolio = append(portfolio[:id], portfolio[id+1:]...)
+	json.NewEncoder(w).Encode(portfolio)
+}
+
+// Update an option
+func updateOpt(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	id, err := strconv.Atoi(params["id"])
+	if err != nil {
+		w.WriteHeader(400)
+		w.Write([]byte("ID could not be converted to integer"))
+		return
+	}
+	if id >= len(portfolio) {
+		w.WriteHeader(404)
+		w.Write([]byte("No option found with specified ID"))
+		return
+	}
+	opt := &portfolio[id]
+	json.NewDecoder(r.Body).Decode(opt)
+	json.NewEncoder(w).Encode(opt)
+}
